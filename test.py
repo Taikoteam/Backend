ceaeea63497dbd3161cf5d10d69ed29cfae3eac3
Paula Santoyo <69481107+A01745312@users.noreply.github.com>
2023-10-12 10:45:40 -0600

    Conexión bd oracle

---
+++ test.py
@@ -0,0 +1,26 @@
+import oracledb
+
+# Contrasena bd
+password = "pruebaTaiko1"
+
+# Contrasena wallet
+wallet_pw= "KnjiTaiko1"
+
+try:
+    print("Conectando a la base de datos...")
+    # Conexion a la bd
+    connection=oracledb.connect(
+     user="admin",
+     password=password,
+     # Servidor al que se quiere conectar (existe high, low, medium, tp, tpurgent)
+     dsn="vrsy5n2bnqg4crf5_medium",
+     # Directorio donde se encuentran las claves
+     config_dir="Docs",
+     wallet_location="Docs",
+     wallet_password=wallet_pw)
+    
+    print("✅ Conexión exitosa a la base de datos Oracle. ")
+    
+
+except oracledb.Error as e:
+    print(f"Error de conexión a la base de datos: {e}")
