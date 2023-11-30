9608471dd7312e089b13d6846578d341196d2255
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-11-30 08:53:03 -0600

    Chatbot discord y whats

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ const database = require('./routes');
 app.use('/database', database);
 app.use('/sms', handleSms);
 
+// Importar la función processMessage de bot_discord.js
+const { processMessage } = require('./bot_discord');
+
+// Middleware para procesar mensajes entrantes
+app.post('/message', async (req, res) => {
+    const incomingMsg = req.body.message;
+    const senderId = req.body.senderId || 'default-sender';
+
+    try {
+        // Procesar el mensaje utilizando la función processMessage de bot.js
+        const response = await processMessage(incomingMsg, senderId);
+
+        // Enviar la respuesta al frontend
+        res.json({ reply: response });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 // Iniciar el servidor
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('El servidor está escuchando en el puerto 3000');
